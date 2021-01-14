75c2400350901e2e31632361fd8614558305af29
Fredxd <frederic.baudouin.45@gmail.com>
2021-01-14 11:17:12 +0100

    fix(db): Fix json data in seo field

--- assets/seo/seo.js
+++ assets/seo/seo.js
@@ -22,7 +22,15 @@ class SeoSnippet {
         this.seoFieldsInputs = {
             title: document.querySelector('#seofields-title'),
             description: document.querySelector('#seofields-description'),
+            keywords: document.querySelector('#seofields-keywords'),
+            shortlink: document.querySelector('#seofields-shortlink'),
+            canonical: document.querySelector('#seofields-canonical'),
+            robots: document.querySelector('#seofields-robots'),
+            og: document.querySelector('#seofields-og'),
         }
+
+        this.seoField = document.querySelector(`[name="${this.container.dataset.seoField}"]`);
+        this.seoData = JSON.parse(this.seoField.value);
 
         this.init();
         this.initEvents();
@@ -30,61 +38,73 @@ class SeoSnippet {
 
     init() {
         const defaultsData = Object.assign({}, this.defaultsData);
+        if(this.seoData.title !== '') {
+            defaultsData.title = this.seoData.title;
+        }
+        if(this.seoData.description !== '') {
+            defaultsData.description = this.seoData.description;
+        }
         this.targetElements.title.innerHTML = defaultsData.title;
         this.targetElements.url.innerHTML = defaultsData.url.replace('REPLACE', defaultsData.slug);
         this.targetElements.description.innerHTML = defaultsData.description;
     }
 
     initEvents() {
-        if(this.inputs.title) {
+        if (this.inputs.title) {
             this.inputs.title.addEventListener('keyup', (e) => {
                 this.changeTarget('title');
             });
         }
-        if(this.seoFieldsInputs.title) {
-            this.seoFieldsInputs.title.addEventListener('keyup', (e) => {
-                this.changeTarget('title');
-            });
-        }
 
-        if(this.inputs.description) {
+        if (this.inputs.description) {
             this.inputs.description.addEventListener('keyup', (e) => {
                 this.changeTarget('description');
             });
         }
-        if(this.seoFieldsInputs.description) {
-            this.seoFieldsInputs.description.addEventListener('keyup', (e) => {
-                this.changeTarget('description');
-            });
-        }
 
-        if(this.inputs.slug) {
+        if (this.inputs.slug) {
             this.inputs.slug.addEventListener('keyup', (e) => {
                 const defaultsData = Object.assign({}, this.defaultsData);
                 this.targetElements.url.innerHTML = defaultsData.url.replace('REPLACE', this.inputs.slug.value);
             });
         }
+
+        Object.keys(this.seoFieldsInputs).forEach((key) => {
+            let eventName = 'keyup';
+            if (key === 'robots') {
+                eventName = 'change';
+            }
+
+            this.seoFieldsInputs[key].addEventListener(eventName, (e) => {
+                this.changeTarget(key);
+            });
+        })
     }
 
     changeTarget(field) {
-        if(
-            this.inputs[field] &&
-            this.inputs[field].value.length > 0 &&
-            (!this.seoFieldsInputs[field] || this.seoFieldsInputs[field].value.length === 0)
-        ) {
-            this.targetElements[field].innerHTML = this.inputs[field].value;
-        } else if (this.seoFieldsInputs[field] && this.seoFieldsInputs[field].value.length > 0) {
-            this.targetElements[field].innerHTML = this.seoFieldsInputs[field].value;
-        } else {
-            this.targetElements[field].innerHTML = this.defaultsData[field];
+        if (field === 'title' || field === 'description') {
+            if (
+                this.inputs[field] &&
+                this.inputs[field].value.length > 0 &&
+                (!this.seoFieldsInputs[field] || this.seoFieldsInputs[field].value.length === 0)
+            ) {
+                this.targetElements[field].innerHTML = this.inputs[field].value;
+            } else if (this.seoFieldsInputs[field] && this.seoFieldsInputs[field].value.length > 0) {
+                this.targetElements[field].innerHTML = this.seoFieldsInputs[field].value;
+            } else {
+                this.targetElements[field].innerHTML = this.defaultsData[field];
+            }
         }
+
+        this.seoData[field] = this.seoFieldsInputs[field].value;
+        this.seoField.value = JSON.stringify(this.seoData);
     }
 }
 
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const seoSnippet = document.querySelector('.seo_snippet');
-    if(seoSnippet) {
+    if (seoSnippet) {
         new SeoSnippet(seoSnippet);
     }
 });
